422085b355601cc4cb14d66cc55b26ddd28056f9
Speks2 <142408255+Speks2@users.noreply.github.com>
2024-01-04 21:06:18 +0100

    neww

--- assets/js/modal.js
+++ assets/js/modal.js
@@ -1,22 +1,48 @@
 
-displayModal("Cookie_container");
+ displayModal("Cookie_container");
 
-function displayModal(id) {
-    document.getElementById(id).style.display = "block";
-    document.getElementById("myBody").classList.toggle("noScroll");
+ function displayModal(id) {
+     document.getElementById(id).style.display = "block";
+     document.getElementById("myBody").classList.toggle("noScroll");
 
-}
+ }
 
-function displayGallery(id, src, desc) {
-    document.getElementById(id).style.display = "block";
-    document.getElementById("desc").innerHTML = desc;
-    document.getElementById("myBody").classList.toggle("noScroll");
+ function displayGallery(id, src, desc) {
+     document.getElementById(id).style.display = "block";
+     document.getElementById("desc").innerHTML = desc;
+     document.getElementById("myBody").classList.toggle("noScroll");
 
      
+ }
+
+ function closeModal(id) {
+     document.getElementById(id).style.display = "none";
+    document.getElementById("myBody").classList.toggle("noScroll");
+ }
+
+
+let myBody = document.getElementById("myBody");
+
+
+function displayModal(elementiD) {
+
+    console.log("kliked " + elementiD);
+
+    let myModalElement = document.getElementById(elementiD);
+    myModalElement.style.display = "block";
+
+    myBody.style.overflow = "hidden";
+
+
 }
 
-function closeModal(id) {
-    document.getElementById(id).style.display = "none";
-    document.getElementById("myBody").classList.toggle("noScroll");
+function closeModal(elementiD) {
+    console.log("kliked " + elementiD);
+
+    let myModalElement = document.getElementById(elementiD);
+    
+    myModalElement.style.display = "none";
+
+    myBody.style.overflow = "scroll";
+
 }
-!important
